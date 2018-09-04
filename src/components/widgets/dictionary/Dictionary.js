ebbb5684ca42b339eb073614b2f94874f8c97e47
Steven Penfield <stevenpenfield9@gmail.com>
2018-09-04 17:34:17 -0600

    front end commit # 1

---
+++ src/components/widgets/dictionary/Dictionary.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Dictionary extends Component {
+	render() {
+		return (
+			<div>
+				Here is where the Dictionary goes.
+			</div>
+		)
+	}
+}
---
+++ src/components/widgets/weather/Weather.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Weather extends Component {
+	render() {
+		return (
+			<div>
+				Here is where the Weather goes.
+			</div>
+		)
+	}
+}
