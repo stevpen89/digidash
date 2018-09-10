781685e362dec8abe0a3ba0b010e2f18c5543957
masonwells <masonwbrockbank@gmail.com>
2018-09-10 15:28:54 -0600

    fixed reset of state on lock toggle

--- src/components/widgets/clock/Clock.js
+++ src/components/widgets/clock/Clock.js
@@ -2,32 +2,45 @@ import React, { Component } from 'react';
 import ClockTheme from './ClockTheme';
 import Clock from 'react-clock';
 import Digital from './Digital'
+import axios from 'axios'
 import './Clock.css'
 
 class WidgetClock extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    const { o1, o2, o3, o4, o5, o6 } = this.props.o
     this.state = {
       miniSettings: false,
-      show: true,
+      show: o1 === 'true' ? true : false,
       date: new Date(),
       theme: `background: rgba(255,255,255,.5)`
     }
   }
 
   componentDidMount() { setInterval(() => this.setState({ date: new Date() }), 1000) }
-  toggleSettings() { this.setState({ miniSettings: !this.state.miniSettings }) }
+  toggleSettings() {
+    const { show } = this.state
+    this.setState({ miniSettings: !this.state.miniSettings })
+    axios.put(`/widget/settings/${this.props.o.master_id}`, {
+      o1: show,
+      o2: null,
+      o3: null,
+      o4: null,
+      o5: null,
+      o6: null,
+    }).then(this.props.updateWidgets())
+  }
   toggle() { this.setState({ show: !this.state.show }) }
 
   render() {
-    console.log('chicken nugget swag', this.props.o.o1)
+    const { show, miniSettings } = this.state;
     return (
       <div className="clock standard-widget">
 
         <ClockTheme theme={this.state.theme} />
         <button className="widget-settings-button" onClick={() => this.toggleSettings()}>•••</button>
 
-        {this.state.miniSettings ?
+        {miniSettings ?
           <div className="widget-settings">
             <div className={!this.state.show ? `checkbox checked theme-color` : `checkbox`} onClick={() => { this.toggle() }}>
               <i class="fas fa-check"></i>
@@ -36,7 +49,7 @@ class WidgetClock extends Component {
           </div> : null
         }
 
-        <div>{this.state.show ? <Clock value={this.state.date} size='200' /> : <Digital />}</div>
+        <div>{show ? <Clock value={this.state.date} size='200' /> : <Digital />}</div>
         <div className="theme-glow"></div>
         <div className="theme-accent"></div>
       </div>
@@ -44,4 +57,4 @@ class WidgetClock extends Component {
   }
 }
 
-export default WidgetClock;
+export default WidgetClock
