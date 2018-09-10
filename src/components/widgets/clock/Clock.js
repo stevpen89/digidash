8234ec7400bed19ee3c230e27fab575dfb8d13a2
masonwells <masonwbrockbank@gmail.com>
2018-09-10 11:54:37 -0600

    passing widgets settings through props through switch statement

--- src/components/widgets/clock/Clock.js
+++ src/components/widgets/clock/Clock.js
@@ -5,25 +5,26 @@ import Digital from './Digital'
 import './Clock.css'
 
 class WidgetClock extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
-      miniSettings : false,
-      show         : true,
-      date         : new Date(),
-      theme        : `background: rgba(255,255,255,.5)`
+      miniSettings: false,
+      show: true,
+      date: new Date(),
+      theme: `background: rgba(255,255,255,.5)`
     }
   }
 
-  componentDidMount() {setInterval(() => this.setState({ date: new Date() }), 1000)}
-  toggleSettings() {this.setState({miniSettings: !this.state.miniSettings})}
-  toggle() {this.setState({show: !this.state.show})}
+  componentDidMount() { setInterval(() => this.setState({ date: new Date() }), 1000) }
+  toggleSettings() { this.setState({ miniSettings: !this.state.miniSettings }) }
+  toggle() { this.setState({ show: !this.state.show }) }
 
   render() {
+    console.log(this.props)
     return (
       <div className="clock standard-widget">
 
-        <ClockTheme theme={this.state.theme}/>
+        <ClockTheme theme={this.state.theme} />
         <button className="widget-settings-button" onClick={() => this.toggleSettings()}>•••</button>
 
         {this.state.miniSettings ?
@@ -35,7 +36,7 @@ class WidgetClock extends Component {
           </div> : null
         }
 
-        <div>{this.state.show ? <Clock value={this.state.date} size='200'/> : <Digital />}</div>
+        <div>{this.state.show ? <Clock value={this.state.date} size='200' /> : <Digital />}</div>
         <div className="theme-glow"></div>
         <div className="theme-accent"></div>
       </div>
