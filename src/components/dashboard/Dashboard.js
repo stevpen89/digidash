d2633af9ae0528552bef4a330888e9917ff61695
masonwells <masonwbrockbank@gmail.com>
2018-09-10 18:18:07 -0600

    drawer functionality and widget display

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -80,19 +80,19 @@ class Dashboard extends Component {
     }
   }
 
-  toolbarSwitch(val){
+  toolbarSwitch(val) {
     switch (val.widget_name) {
       case 'Search': return <i className="fas fa-search"></i>;
       case 'Dictionary': return <i className="fas fa-book-open"></i>;
       case 'Note': return <i className="fas fa-sticky-note"></i>;
       case 'Clock': return <i className="fas fa-clock"></i>;
-      case 'Weather': return <i className="fas fa-sun"></i>;
+      case 'Weather': return <i className="fas fa-cloud"></i>;
       default: return 'defaulted';
     }
   }
 
-  addWidget(val){
-    
+  addWidget(val) {
+
   }
 
   render() {
@@ -146,8 +146,6 @@ class Dashboard extends Component {
             </div>
           ))}
         </ReactGridLayout>
-        <Weather />
-        <Note/>
 
         {/* DRAWER */}
         <div style={{
@@ -164,19 +162,56 @@ class Dashboard extends Component {
           transition: `.5s`
         }}>
           <div className="drawer">
-            <button className="filled-button">Drawer</button>
+            <div onClick={() => {
+              axios.post(`/widget/${this.props.user_id}`, {
+                user_id: this.props.user_id,
+                widget_name: 'Search', x: 0, y: 0, w: 26, h: 4
+              })
+                .then(() => this.updateWidgets())
+            }
+            }><i class="fas fa-search"></i><a>Search</a></div>
+            <div onClick={() => {
+              axios.post(`/widget/${this.props.user_id}`, {
+                user_id: this.props.user_id,
+                widget_name: 'Weather', x: 0, y: 0, w: 10, h: 10
+              })
+                .then(() => this.updateWidgets())
+            }
+            }><i class="fas fa-cloud"></i><a>Weather</a></div>
+            <div onClick={() => {
+              axios.post(`/widget/${this.props.user_id}`, {
+                user_id: this.props.user_id,
+                widget_name: 'Dictionary', x: 0, y: 0, w: 10, h: 10
+              })
+                .then(() => this.updateWidgets())
+            }
+            }><i class="fas fa-book-open"></i><a>Dictionary</a></div>
+            <div onClick={() => {
+              axios.post(`/widget/${this.props.user_id}`, {
+                user_id: this.props.user_id,
+                widget_name: 'Clock', x: 0, y: 0, w: 10, h: 15, o1: 'true'
+              })
+                .then(() => this.updateWidgets())
+            }
+            }><i class="fas fa-clock"></i><a>Clock</a></div>
+            <div onClick={() => {
+              axios.post(`/widget/${this.props.user_id}`, {
+                user_id: this.props.user_id,
+                widget_name: 'Note', x: 0, y: 0, w: 10, h: 10
+              })
+                .then(() => this.updateWidgets())
+            }
+            }><i class="fas fa-sticky-note"></i><a>Note</a></div>
           </div>
           <div className="drawer-background"></div>
         </div>
 
         {/* TOOLBAR */}
-        <div className={this.state.locked ? 
-          'toolbar toolbar-open' :
-           'toolbar'}>
-        <div style={{display:"flex", color:"red"}}>{this.state.widgets ?
-           this.state.widgets.map((val)=>{return <div style={{fontSize:"30px", margin:"0 10px"}} onClick={()=>this.addWidget(val.widget_name)}>{this.toolbarSwitch(val)}</div>}) :
+        <div className={this.state.locked ? 'toolbar toolbar-open' : 'toolbar'}>
+          <div style={{ display: "flex" }}>{this.state.widgets ?
+            this.state.widgets.map((val) => { return <div style={{ fontSize: "30px", margin: "0 10px" }} onClick={() => this.addWidget(val.widget_name)}>{this.toolbarSwitch(val)}</div> }) :
             ''}</div>
-          <div><button onClick={() => this.setState({ drawerOpen: !this.state.drawerOpen })}><i class="fas fa-plus-square"></i></button></div>
+          <div><button onClick={() => this.setState({ drawerOpen: !this.state.drawerOpen })}>{this.state.drawerOpen ? <i class="fas fa-minus-square"></i> : <i class="fas fa-plus-square"></i>}</button></div>
         </div>
       </div>
     )
