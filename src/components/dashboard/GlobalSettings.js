10b0ece6b5ccad0cdcb355d747682c657d48188d
Steven Penfield <stevenpenfield9@gmail.com>
2018-09-11 20:23:25 -0600

    added the global settings thing(in a css file this time)

--- src/components/dashboard/GlobalSettings.js
+++ src/components/dashboard/GlobalSettings.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
+import './GlobalSettings.css'
 
 export default class GlobalSettings extends Component {
 	render() {
 		return (
 			<div className="globalBig">
 				
-				<div style={{marginBottom: '40px' ,display:"flex", justifyContent:'center',color:"white"}}>
-					<h1 style={{color:"white"}}>S E T T I N G S</h1>
+				<div className="globalSmall">
+					<h1 className="globalItem">S E T T I N G S</h1>
 				</div>
 				
-				<div style={{ height: '100%',color:"white",display:"flex", justifyContent: 'space-evenly'}}>
-						<div style={{width:"50%", display:"flex", justifyContent: 'center', borderRight:".05px solid white"}}>
-							<h4 style={{color:"white"}}>THEMES</h4>
-						</div>
-						
-						<div style={{width:"50%", display:"flex", justifyContent: 'center'}}>
-							<h4 style={{color:"white"}}>LAYOUT</h4>
-						</div>
+				<div className="globalMain">
+					<div className="globalThemes">
+						<h4 className="globalItem">THEMES</h4>
+					</div>
+					
+					<div className="globalLayout">
+						<h4 className="globalItem">LAYOUT</h4>
+					</div>
 				</div>
-			
 			</div>
 
 		)
